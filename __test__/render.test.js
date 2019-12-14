acd3ec5499080efeffb4459d9651e5c9c3421233
Yuito Murase <yuito.murase@gmail.com>
2019-12-14 18:04:54 +0900

    #4 Add tests for Drive and refactor directory structure

--- __test__/render.test.js
+++ __test__/render.test.js
@@ -5,7 +5,7 @@ expect.extend({ toMatchImageSnapshot });
 
 shell.cd('__test__');
 
-const expectRenderOutputToMatchSnapshot = (filename) => {
+const compareRenderOutputWithSnapshot = (filename) => {
   return (done) => {
     render(filename, (err, image) => {
       expect(err).toBeFalsy();
@@ -17,21 +17,26 @@ const expectRenderOutputToMatchSnapshot = (filename) => {
 
 afterAll(() => {
   console.log(shell.pwd().toString());
-  shell.rm('*.pdf', '*.satysfi-aux');
+  shell.rm('**/*.pdf', '**/*.satysfi-aux');
 })
 
 test('Confirm that satysfi is installed', () => {
   expect(shell.exec('satysfi -v').code).toBe(0);
 })
 
-test('Render StdJa', expectRenderOutputToMatchSnapshot('stdja'))
+test('Render StdJa', compareRenderOutputWithSnapshot('src/stdja'))
 
 describe('Render Derive', () => {
-  it('derive', expectRenderOutputToMatchSnapshot('derive-derive'));
+  it('derive', compareRenderOutputWithSnapshot('src/derive/derive'));
+  it('assume', compareRenderOutputWithSnapshot('src/derive/assume'));
+  it('by', compareRenderOutputWithSnapshot('src/derive/by'));
+  it('by and byOp', compareRenderOutputWithSnapshot('src/derive/by-and-byop'));
+  it('from', compareRenderOutputWithSnapshot('src/derive/from'));
+  it('dotted line', compareRenderOutputWithSnapshot('src/derive/dotted-line'));
 })
 
 const render = (filename, callback) => {
-  expect(shell.exec(`satysfi ${filename}.saty`).code).toBe(0);
+  expect(shell.exec(`satysfi ${filename}.saty`, {silent: true}).code).toBe(0);
 
   gm(`${filename}.pdf`)
     .selectFrame(0)
